8cc054b421ec83af59b8d69103d100f385967fd3
MauricioArgu <Mauro66Arg@gmail.com>
2020-08-11 11:34:06 -0600

    Commit prueba

--- ProyectoEstructurada/Program.cs
+++ ProyectoEstructurada/Program.cs
@@ -11,6 +11,7 @@ namespace ProyectoEstructurada
         /// <summary>
         /// Punto de entrada principal para la aplicación.
         /// </summary>
+        /// Prueba
         [STAThread]
         static void Main()
         {
